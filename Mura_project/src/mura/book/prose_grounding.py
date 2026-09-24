60f6e586c288ebe650411ad110d17f7b948cb34a
Erbosyn <shaimardanerbosyn9@gmail.com>
2026-09-24 12:57:21 +0500

    style: carry forward backend lint fixes

--- Mura_project/src/mura/book/prose_grounding.py
+++ Mura_project/src/mura/book/prose_grounding.py
@@ -8,9 +8,9 @@ against the immutable selected-source snapshot.
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 import re
 import unicodedata
+from dataclasses import dataclass
 
 from mura.domain.book_models import BookSourceSnapshot
 
